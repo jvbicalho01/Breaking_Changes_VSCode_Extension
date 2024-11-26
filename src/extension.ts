73bb767c09bdc2b94116119eec02d02f3a3a9f8c
João Vitor Bicalho <joaovitorbicalho.dasilva@gmail.com>
2024-11-25 21:08:59 -0300

    add primeiro "protótipo" de extensão que identifica uma chamada de função

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,33 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    // Registrar um comando que será executado ao abrir o arquivo
+    let disposable = vscode.commands.registerCommand('extension.breakingChange', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
+        const document = editor.document;
+        const text = document.getText();
+        const diagnostics: vscode.Diagnostic[] = [];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from HelloWorld!');
-	});
+        const functionCallRegex = /\b\w+\s*\(.*?\)/g;
+        let match;
+        while ((match = functionCallRegex.exec(text)) !== null) {
+            const startPos = document.positionAt(match.index);
+            const endPos = document.positionAt(match.index + match[0].length);
+            const range = new vscode.Range(startPos, endPos);
 
-	context.subscriptions.push(disposable);
+            const diagnostic = new vscode.Diagnostic(range, `Função chamada: ${match[0]}`, vscode.DiagnosticSeverity.Warning);
+            diagnostics.push(diagnostic);
+        }
+
+        const diagnosticCollection = vscode.languages.createDiagnosticCollection('breakingChanges');
+        diagnosticCollection.set(document.uri, diagnostics);
+    });
+
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
